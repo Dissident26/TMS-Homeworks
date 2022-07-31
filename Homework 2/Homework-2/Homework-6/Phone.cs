bba2865caddc33e72d8bf6d460276180c7a25c69
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-07-31 18:10:00 +0300

    add homework #6

---
+++ Homework 2/Homework-2/Homework-6/Phone.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework_6
+{
+    public class Phone
+    {
+        public string Number { get; set; }
+        public string Model { get; }
+        public double Weight { get; }
+
+
+        public Phone(string number, string model, double weight)
+        {
+            Number = number;
+            Model = model;
+
+            if(weight < 0)
+            {
+                Weight = 0;
+                return;
+            }
+            Weight = weight;
+        }
+
+        public void RecieveCall(string callerName)
+        {
+            Console.WriteLine("Вам звонит {0}", callerName);
+        }
+    }
+}
---
+++ Homework 2/Homework-2/Homework-6/Program.cs
@@ -0,0 +1,20 @@
+﻿using Homework_6;
+
+Phone[] phoneArray = new Phone[]
+{
+    new("123321", "Motorolla", 200d),
+    new("326234", "Nokia", 200d),
+    new("6796796", "Siemens", -100d),
+    new("4757", "Sony", -200d)
+};
+
+foreach (var phone in phoneArray)
+{
+    Console.WriteLine(@"
+        Номер телефона: {0};
+        Модель: {1};
+        Вес: {2} гр.;
+", phone.Number, phone.Model, phone.Weight);
+
+    phone.RecieveCall("Президент");
+}
