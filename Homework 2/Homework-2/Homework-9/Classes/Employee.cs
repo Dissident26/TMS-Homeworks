7af47e707119e1a00bdd85e07be1c97a27adc0f9
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-08-15 15:33:01 +0300

    add #9 homework

---
+++ Homework 2/Homework-2/Homework-9/Classes/Employee.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework_9.Classes
+{
+    public delegate int GetSalary(Employee employee);
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int WorkingExperience { get; set; }
+        public bool HasHighEduacation { get; set; }
+
+        public Employee(string name, int age, int workingExperience, bool hasHighEducation)
+        {
+            Name = name;
+            Age = age;
+            WorkingExperience = workingExperience;
+            HasHighEduacation = hasHighEducation;
+        }
+
+       public static void GetSalary(List<Employee> employeesList)
+        {
+            foreach (var employee in employeesList)
+            {
+                var salaryModificator = employee.HasHighEduacation ? 1250 : 1000;
+                var employeesSalary = (employee.WorkingExperience + 1) * salaryModificator;
+
+                Console.WriteLine($"Работник: {employee.Name}, Зарплата: {employeesSalary} условных рублей");
+            }
+        }
+
+        public static void GetSalary(List<Employee> employeesList, GetSalary getSalary)
+        {
+            foreach (var employee in employeesList)
+            {
+                var employeesSalary = getSalary(employee);
+
+                Console.WriteLine($"Работник: {employee.Name}, Зарплата: {employeesSalary} условных рублей");
+            }
+        }
+    }
+}
