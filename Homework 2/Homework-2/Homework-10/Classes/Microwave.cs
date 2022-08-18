fa9c221638114a62550d58cfbc118e942115e297
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-08-18 12:03:07 +0300

    add #10 homework

---
+++ Homework 2/Homework-2/Homework-10/Classes/Microwave.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_10.Classes
+{
+    public delegate void NotifyWarmUpCompleted(string dishName);
+    internal class Microwave
+    {
+        public event NotifyWarmUpCompleted? WarmUpCompleted;
+        public string Name { get; set; }
+        public Microwave(string name)
+        {
+            Name = name;
+        }
+
+        public void WarmUp(string dishName)
+        {
+            Console.WriteLine($"Подогреваю {dishName}");
+            WarmUpCompleted?.Invoke(dishName);
+        }
+    }
+}
---
+++ Homework 2/Homework-2/Homework-10/Program.cs
@@ -0,0 +1,8 @@
+﻿using Homework_10.Classes;
+
+var microwave1 = new Microwave("1");
+
+microwave1.WarmUpCompleted += (string dishName) => Console.WriteLine($"Посетитель #1, Ваша еда готова: {dishName}");
+microwave1.WarmUpCompleted += (string dishName) => Console.WriteLine($"Посетитель #2, Ваша еда готова: {dishName}");
+
+microwave1.WarmUp("Пирожок");
