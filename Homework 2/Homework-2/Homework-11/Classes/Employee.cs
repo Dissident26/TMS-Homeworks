aaf18ce05150076aad975da202ab8e12413c44a7
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-08-24 19:02:30 +0300

    add #11 homework

---
+++ Homework 2/Homework-2/Homework-11/Classes/Employee.cs
@@ -0,0 +1,69 @@
+﻿namespace Homework_11.Classes
+{
+    internal class Employee
+    {
+        private string _name = String.Empty;
+        private int _age;
+        private double _salary;
+        public string Name
+        {
+            get { return _name; }
+            set {
+                if (!IsNameValid(value))
+                {
+                    throw new ArgumentException("Invalid name! Name should be neither null, empty string, nor consist of more than 100 characters!");
+                }
+                _name = value;
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set {
+                if (!IsAgeValid(value))
+                {
+                    throw new ArgumentException();
+                }
+                _age = value;
+            }
+        }  
+        public double Salary
+        {
+            get { return _salary; }
+            set {
+                if (!IsSalaryValid(value))
+                {
+                    throw new ArgumentException();
+                }
+                _salary = value;
+            }
+        }
+        public Employee(string name, int age, double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        private bool IsNameValid(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException();
+            }
+            if (name.Length > 100)
+            {
+                throw new ArgumentException();
+            }
+            return true;
+        }
+        private bool IsAgeValid(int age)
+        {
+            return age > 0 && age >= 18;
+        }
+        private bool IsSalaryValid(double salary)
+        {
+            return salary > 0;
+        }
+    }
+}
---
+++ Homework 2/Homework-2/Homework-11/Program.cs
@@ -0,0 +1,21 @@
+﻿using Homework_11.Classes;
+
+var emloyee = new Employee("Работник #1", 21, 1000);
+
+PotentialyDangerousChanges(() =>
+{
+    emloyee.Age = 12;   //insert some not valid data here
+});
+
+static void PotentialyDangerousChanges(Action action)
+{
+    try
+    {
+        action();
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine(ex.StackTrace);
+    }
+}
