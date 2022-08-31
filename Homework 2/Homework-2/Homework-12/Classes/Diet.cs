327f1e5b51546e632767b42a433af65da9e20fae
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-08-31 18:25:46 +0300

    add some classes

---
+++ Homework 2/Homework-2/Homework-12/Classes/Diet.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework_12.Classes
+{
+    public class Diet
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Code { get; set; }
+        public List<Ingridient> Ingridients { get; set; }
+
+        public Diet(string name, string author, string code, List<Ingridient> ingridients)
+        {
+            Name = name;
+            Author = author;
+            Code = code;
+            Ingridients = ingridients;
+        }
+    }
+}
---
+++ Homework 2/Homework-2/Homework-12/Classes/Ingridient.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework_12.Classes
+{
+    public class Ingridient
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double EnergyCost { get; set; }
+        public string Manufacturer { get; set; }
+
+        public Ingridient(string name, double price, double energyCost, string manufacturer)
+        {
+            Name = name;
+            Price = price;
+            EnergyCost = energyCost;
+            Manufacturer = manufacturer;
+        }
+    }
+}
